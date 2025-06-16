91362be4148c9286df775999840820411c9b78ff
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-06-17 02:20:47 +0530

    implement with password hashing and login verification

--- src/main/java/com/drawingcanvas/service/impl/AuthServiceImpl.java
+++ src/main/java/com/drawingcanvas/service/impl/AuthServiceImpl.java
@@ -1,2 +1,77 @@
-package com.drawingcanvas.service.impl;public class AuthServiceImpl {
+package com.drawingcanvas.service.impl;
+
+import com.drawingcanvas.config.DatabaseConnector;
+import com.drawingcanvas.model.User;
+import com.drawingcanvas.service.AuthService;
+import com.drawingcanvas.util.PasswordUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AuthServiceImpl implements AuthService {
+
+    @Override
+    public boolean register(User user) {
+
+        //query to insert a new user
+        String sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+
+        //try connection with Prepared statements
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            // Set the values
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, PasswordUtil.hashPassword(user.getPassword()));
+
+            // Execute query
+            int rowsInserted = stmt.executeUpdate();
+
+            return rowsInserted > 0;
+
+        } catch (SQLException e) {
+
+            // Print error when have error
+            System.err.println("Registration failed: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public User login(String username, String password) {
+
+        //query find a user
+        String sql = "SELECT * FROM users WHERE username = ?";
+
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            // Set the username
+            stmt.setString(1, username);
+
+            //Execute the query
+            ResultSet rs = stmt.executeQuery();
+
+            //check password & return details if correct
+            if (rs.next()) {
+                String storedHash = rs.getString("password");
+
+                if (PasswordUtil.checkPassword(password, storedHash)) {
+                    return new User(
+                            rs.getInt("id"),
+                            rs.getString("username"),
+                            rs.getString("email"),
+                            storedHash
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Login failed: " + e.getMessage());
+        }
+
+        return null;
+    }
 }
