8e102407935838255174ef3241d06433db113b56
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-06-17 02:18:07 +0530

    add hashing and checking passwords using BCrypt

--- src/main/java/com/drawingcanvas/util/PasswordUtil.java
+++ src/main/java/com/drawingcanvas/util/PasswordUtil.java
@@ -1,2 +1,16 @@
-package com.drawingcanvas.util;public class PasswordUtil {
+package com.drawingcanvas.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+
+    //convert plain password -> hashed password
+    public static String hashPassword(String plainPassword) {
+        return BCrypt.hashpw(plainPassword, BCrypt.gensalt(12));
+    }
+
+    //check plain password == hashed password during login
+    public static boolean checkPassword(String plainPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainPassword, hashedPassword);
+    }
 }
