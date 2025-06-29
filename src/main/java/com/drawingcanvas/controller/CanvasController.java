a885b70700943610d1c0eb9b73c6ea325afe0a6e
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-06-29 14:34:46 +0530

    Create controller for canvas

--- src/main/java/com/drawingcanvas/controller/CanvasController.java
+++ src/main/java/com/drawingcanvas/controller/CanvasController.java
@@ -1,2 +1,33 @@
-package com.drawingcanvas.controller;public class CanvasController {
+package com.drawingcanvas.controller;
+
+import com.drawingcanvas.service.impl.DrawingServiceImpl;
+import com.drawingcanvas.service.intf.DrawingService;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class CanvasController {
+
+    @FXML private Canvas canvas;
+    @FXML private Button lineTool, rectTool, ellipseTool, freehandTool;
+
+    private final DrawingService drawingService = new DrawingServiceImpl();
+
+    @FXML
+    public void initialize() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawingService.setGraphicsContext(gc);
+
+        canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, drawingService::handleMousePressed);
+        canvas.addEventHandler(MouseEvent.MOUSE_DRAGGED, drawingService::handleMouseDragged);
+        canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, drawingService::handleMouseReleased);
+    }
+
+    @FXML private void selectLineTool()     { drawingService.setTool("LINE"); }
+    @FXML private void selectRectTool()     { drawingService.setTool("RECTANGLE"); }
+    @FXML private void selectEllipseTool()  { drawingService.setTool("ELLIPSE"); }
+    @FXML private void selectFreehandTool() { drawingService.setTool("FREEHAND"); }
+    @FXML private void clearCanvas()        { drawingService.clearCanvas(); }
 }
