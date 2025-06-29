2e514babdeae1f26bade6651f97553861c1e11df
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-06-29 14:36:01 +0530

    change starting root file

--- src/main/java/com/drawingcanvas/MainApplication.java
+++ src/main/java/com/drawingcanvas/MainApplication.java
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/com/drawingcanvas/fxml/new-login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 780, 460);
+        scene.getStylesheets().add(getClass().getResource("/com/drawingcanvas/css/newLoginView.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
